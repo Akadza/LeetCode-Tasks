c0f232f07c43cdefd8a22618d62ec6a961f03af8
Alexander Byakov <byakov-a55@mail.ru>
2023-07-04 00:40:59 +0300

    Task about <PalindromeNumber> was added

---
+++ PalindromeNumber/PalindromeNumber.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::endl;
+
+
+class Solution {
+public:
+    static bool isPalindrome(long int x) {
+        long int y = 0, a = x;
+        if (x < 0) return false;
+        while (a != 0) {
+            y = y * 10 + (a % 10);
+            a /= 10;
+        }
+        if (y == x) return true;
+        else return false;
+    }
+};
+
+
+int main()
+{
+    cout << Solution::isPalindrome(1211121) << endl;
+
+    return 0;
+}
