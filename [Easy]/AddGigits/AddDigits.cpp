5a16ec4b70a12dc6991efa58a42a391dffbe061b
Alexander Byakov <byakov-a55@mail.ru>
2023-07-06 17:19:25 +0300

    Task: AddDigits [easy] was added

---
+++ [Easy]/AddGigits/AddDigits.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+
+class Solution {
+public:
+    static int addDigits(int num) {
+        if (num == 0) return 0;
+        else if (num % 9 == 0) return 9;
+        else return num % 9;
+    }
+};
+
+
+int main()
+{
+    cout << Solution::addDigits(98);
+    return 0;
+}
