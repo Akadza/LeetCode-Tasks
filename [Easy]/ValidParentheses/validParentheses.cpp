6389cc736150f44e7e3c8bd6d8e06a11e270e21c
Alexander Byakov <byakov-a55@mail.ru>
2023-07-09 16:06:50 +0300

    Task: ValidParentheses was added

---
+++ [Easy]/ValidParentheses/validParentheses.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <stack>
+
+
+using std::cout;
+using std::endl;
+using std::string;
+
+
+class Solution {
+public:
+    static bool isValid(string& s) {
+        std::stack<char> brackets;
+        for (char c : s) {
+            if (c == '(') brackets.push(')');
+            else if (c == '{') brackets.push('}');
+            else if (c == '[') brackets.push(']');
+            else if (brackets.empty() || brackets.top() != c) return false;
+            else brackets.pop();
+        }
+        return brackets.empty();
+    }
+};
+
+int main()
+{
+    string s = "{}{}{}[]";
+    cout << Solution::isValid(s) << endl;
+    
+    std::stack<int> stack;
+    cout << "Example " << stack.empty();
+
+
+    return 0;
+}
