16a3ce8a6c2fcf318827d93ca9906278f62ed37f
Alexander Byakov <byakov-a55@mail.ru>
2023-07-11 14:36:53 +0300

    Task [easy]: RemoveDublicatesFromSortedArray was added

---
+++ [Easy]/RemoveDublicatesFromSortedArray/RemoveDublicatesFromSortedArray.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+
+class Solution {
+public:
+    static int removeDuplicates(vector<int>& nums) {
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums.erase(nums.begin() + i + 1);
+                i--;
+            }
+        }
+        return nums.size();
+    }
+};
+
+
+int main()
+{
+    vector<int> nums = { 1, 1, 2 };
+    cout << Solution::removeDuplicates(nums);
+
+    return 0;
+}
