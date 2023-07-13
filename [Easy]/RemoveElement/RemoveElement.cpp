e1fa0f21495c6a78f4c2092665fd11ef050fdcd5
Alexander Byakov <byakov-a55@mail.ru>
2023-07-13 17:13:04 +0300

    Task [Easy]: Remove Element was added

---
+++ [Easy]/RemoveElement/RemoveElement.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+
+class Solution {
+public:
+    static int removeElement(vector<int>& nums, int val) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == val) {
+                nums.erase(nums.begin() + i);
+                i--;
+            }
+        }
+        return nums.size();
+    }
+};
+
+
+int main()
+{
+    vector<int> nums = { 5, 2, 2, 8, 16 };
+    cout << Solution::removeElement(nums, 2) << endl;
+}
+
